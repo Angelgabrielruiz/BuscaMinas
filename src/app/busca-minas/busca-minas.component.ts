562bd8b5bfa3c873a910d06a1934edb5691e9e45
Angelgabrielruiz <aa09angel@gmail.com>
2024-11-05 11:54:27 -0600

    my first commit

---
+++ src/app/busca-minas/busca-minas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-minas',
+  templateUrl: './busca-minas.component.html',
+  styleUrls: ['./busca-minas.component.css']
+})
+export class BuscaMinasComponent {
+ 
+}
